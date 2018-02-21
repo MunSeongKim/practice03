99cac92d8b54062605d53637cf022214f8008937
bit <bit@DESKTOP-518UE4O>
2018-02-21 15:43:51 +0900

    p2 solved

--- src/prob02/GoodsApp.java
+++ src/prob02/GoodsApp.java
@@ -10,6 +10,16 @@ public class GoodsApp {
 
 		Goods[] goods = new Goods[COUNT_GOODS];
 		
+		for(int i = 0; i < COUNT_GOODS; i++){
+			String input = scanner.nextLine();
+			String[] tmp = input.split(" ");
+			
+			goods[i] = new Goods(tmp[0], Integer.parseInt(tmp[1]), Integer.parseInt(tmp[2]));
+		}
+		
+		for(Goods g : goods){
+			g.showInfo();
+		}
 
 		
 		scanner.close();
