98479ab893ee1a441ad42dbb6874cd0f744c3f41
bit <bit@DESKTOP-518UE4O>
2018-02-21 15:50:26 +0900

    p3 solved

--- src/prob03/CurrencyConverterTest.java
+++ src/prob03/CurrencyConverterTest.java
@@ -3,7 +3,11 @@ package prob03;
 public class CurrencyConverterTest {
 
 	public static void main(String[] args) {
-		//  환율을 세팅 합니다.
+		//  환율을 세팅 합니다. (KRW/$1)
+		CurrencyConverter.setRate( 1000 );
+		System.out.println("백만원은 " + CurrencyConverter.toDollar( 1000000 ) + "달러입니다.");
+		System.out.println("백달러는 " + CurrencyConverter.toKRW( 100 ) + "원입니다.");
+		
 	}
 
 }
